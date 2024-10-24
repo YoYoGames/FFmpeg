e9773b7e2a5ea3c5c090ba06deeb3ffd07d62f20
ksuchitra532 <ksuchitra532@gmail.com>
2024-10-24 12:45:31 +0100

    fixes for path

--- .github/actions/linux/linux_build_script.py
+++ .github/actions/linux/linux_build_script.py
@@ -5,7 +5,7 @@ github_workspace = os.getenv('GITHUB_WORKSPACE')
 
 base_lib_dir = os.path.join(github_workspace, 'out/full_deploy/host')
 
-bin_base_dir = os.path.join(github_workspace, 'out/full_deploy/host/ffmpeg')
+base_bin_dir = os.path.join(github_workspace, 'out/full_deploy/host/ffmpeg')
 
 target_dir = os.path.join(github_workspace, 'out/linux')
 
@@ -31,21 +31,21 @@ files_to_copy = [
 os.makedirs(target_dir, exist_ok=True)
 
 def find_binary_file(binary_name):
-    for root, dirs, files in os.walk(bin_base_dir):
+    for root, files in os.walk(base_bin_dir):
         for file in files:
             if binary_name == file:
                 return os.path.join(root, file)
     return None
 
 def find_library_file(library_name):
-    for root, dirs, files in os.walk(base_lib_dir):
+    for root, files in os.walk(base_lib_dir):
         for file in files:
             if library_name in file:
                 return os.path.join(root, file)
     return None
 
 for filename in files_to_copy:
-    if filename in ["ffmpeg", "ffprobe", "libcharset.1.dylib"]:
+    if filename in ["ffmpeg", "ffprobe", "libcharset.so.1"]:
         source_path = find_binary_file(filename) 
     else:
         source_path = find_library_file(filename) 
--- .github/actions/mac/mac_build_script.py
+++ .github/actions/mac/mac_build_script.py
@@ -5,7 +5,7 @@ github_workspace = os.getenv('GITHUB_WORKSPACE')
 
 base_lib_dir = os.path.join(github_workspace, 'out/full_deploy/host')
 
-bin_base_dir = os.path.join(github_workspace, 'out/full_deploy/host/ffmpeg')
+base_bin_dir = os.path.join(github_workspace, 'out/full_deploy/host/ffmpeg')
 
 target_dir = os.path.join(github_workspace, 'out/macos')
 
@@ -31,14 +31,14 @@ files_to_copy = [
 os.makedirs(target_dir, exist_ok=True)
 
 def find_binary_file(binary_name):
-    for root, dirs, files in os.walk(bin_base_dir):
+    for root, files in os.walk(base_bin_dir):
         for file in files:
             if binary_name == file:
                 return os.path.join(root, file)
     return None
 
 def find_library_file(library_name):
-    for root, dirs, files in os.walk(base_lib_dir):
+    for root, files in os.walk(base_lib_dir):
         for file in files:
             if library_name in file:
                 return os.path.join(root, file)
